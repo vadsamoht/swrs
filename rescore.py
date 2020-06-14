8686496355a53f39d3468596079400d353cb76d8
vadsamoht <1444672+vadsamoht@users.noreply.github.com>
2020-06-14 19:18:17 +0000

    SWRS: scoring works, need to add into DB and fix API bug

--- rescore.py
+++ rescore.py
@@ -12,6 +12,37 @@ fastest_level_multiplier = 3
 DATABASE = './runs_db.sqlite'
 DATESTAMP = date.today().strftime("%Y%m%d")
 
+il_platform_names = ["N64",
+                     "PC"]
+il_medal_names = ["Any Medal",
+                  "Gold Medal"]
+il_category_names = ["X-Wing",
+                     "Y-Wing",
+                     "A-Wing",
+                     "V-Wing",
+                     "Speeder",
+                     "Millennium Falcon",
+                     "TIE Interceptor",
+                     "Naboo Starfighter"]
+il_level_names = ["Ambush at Mos Eisley",
+                  "Rendezvous on Barkhesh",
+                  "The Search for the Nonnah",
+                  "Defection at Corellia",
+                  "Liberation of Gerrard V",
+                  "The Jade Moon",
+                  "Imperial Construction Yards",
+                  "Assault on Kile II",
+                  "Rescue on Kessel",
+                  "Prisons of Kessel",
+                  "Battle Above Taloraan",
+                  "Escapt from Fest",
+                  "Blockade on Chandrila",
+                  "Raid on Sullust",
+                  "Moff Seerdon's Revenge",
+                  "The Battle of Calamari",
+                  "Beggar's Canyon",
+                  "The Death Star Trench Run",
+                  "The Battle of Hoth"]
 
 def calc_score(position, maximum):
 	if position == 1:
@@ -22,7 +53,6 @@ def calc_score(position, maximum):
 		return maximum-5
 	else:
 		return maximum-(position+2)
-
 
 def generate_scores(q_platform, q_level, q_category, q_medal):
 	# Create connection to the DB
@@ -43,7 +73,10 @@ def generate_scores(q_platform, q_level, q_category, q_medal):
 			print(il_leaderboard[i])
 
 	number_players = len(il_leaderboard)
-	maximum_points = prorated_maxima[number_players-1]
+	if number_players > len(prorated_maxima):
+		maximum_points = prorated_maxima[-1]
+	else:
+		maximum_points = prorated_maxima[number_players-1]
 
 	if debug:
 		print("Number of players:", number_players)
@@ -63,11 +96,36 @@ def generate_scores(q_platform, q_level, q_category, q_medal):
 
 
 
-q_platform = "N64"
-q_level = "The Death Star Trench Run"
-q_category = "Y-Wing"
-q_medal = "Any Medal"
+for q_platform in il_platform_names:
+	for q_medal in il_medal_names:
+		for q_level in il_level_names:
+			#q_category = "Y-Wing"
+			level_master_list = []
+			for q_category in il_category_names:
 
-scoreboard = generate_scores(q_platform, q_level, q_category, q_medal)
-for i in scoreboard:
-	print(i)
+				#print(q_level, ":", q_category, "("+q_platform+", "+q_medal+")")
+				scoreboard = generate_scores(q_platform, q_level, q_category, q_medal)
+
+				level_master_list.append(scoreboard)
+				
+			fastest_time = 9999
+			fastest_idx = 0
+			fastest_unique = False
+			for i in range(len(level_master_list)):
+				if level_master_list[i]:
+					#print(level_master_list[i][0][1])
+					if level_master_list[i][0][1] < fastest_time:
+						fastest_time = level_master_list[i][0][1]
+						fastest_idx = i
+						fastest_unique = True
+					elif level_master_list[i][0][1] == fastest_time:
+						fastest_unique = False
+			if fastest_unique:
+				for i in level_master_list[fastest_idx]:
+					#print("WOO", i, i[3])
+					i[3] = i[3] * fastest_level_multiplier
+
+			if True:
+				print("\n" + q_level + ": (" + q_platform + ", " + q_medal + ")")
+				for i in level_master_list:
+					print(i)
